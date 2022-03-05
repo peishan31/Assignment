0286ee4f123efa196260b869820bf377e1409093
Lin Peishan <linpeishann@gmail.com>
2022-03-05 11:45:02 +0800

    Making Task Desc more dynamic

--- Q2/script.js
+++ Q2/script.js
@@ -1,29 +1,58 @@
 function newtask(el) {
-	var taskArray = [];
-	var matches = document.getElementsByClassName("divtask");
-	for (var i = 0, l = matches.length; i < l; i++) 
-		taskArray.push(matches[i].getAttribute("id"));
-	
-	if( taskArray.length ) {
-		var nexttask = Math.max.apply(null, taskArray) + 1;
-	} else {
-		var nexttask = 1;
-	}
-	
-	var newDiv = document.createElement("div");	
-	newDiv.id = nexttask;
-	newDiv.className = 'divtask';
-	newDiv.draggable = 'true';
-	newDiv.addEventListener('dragstart', function() {drag(event)}, false);
-	
-	var taskcontent = document.createElement('P');
-	var t = document.createTextNode("Task description");
-	taskcontent.appendChild(t);     
-	taskcontent.className = 'taskcontent';
-	taskcontent.contentEditable = "true";
-	newDiv.appendChild(taskcontent);
-	
-    document.getElementsByClassName("card-body")[0].appendChild(newDiv);
+    div1 = document.querySelectorAll(".div1 .divtask").length + 1
+    // div2 = document.querySelectorAll(".div2 .divtask").length
+    // div3 = document.querySelectorAll(".div3 .divtask").length
+
+    if (div1 <= 3) {
+        // console.log("div1: "+div1)
+        // console.log("div2: "+div2)
+        // console.log("div3: "+div3)
+        var taskDesc = prompt("Enter task:");
+
+        if (taskDesc!=null || taskDesc!="") {
+            var taskArray = [];
+            var matches = document.getElementsByClassName("divtask");
+            for (var i = 0, l = matches.length; i < l; i++) 
+                taskArray.push(matches[i].getAttribute("id"));
+            
+            if( taskArray.length ) {
+                var nexttask = Math.max.apply(null, taskArray) + 1;
+            } else {
+                var nexttask = 1;
+            }
+            
+            var newDiv = document.createElement("div");	
+            newDiv.id = nexttask;
+            newDiv.className = 'divtask';
+            newDiv.draggable = 'true';
+            newDiv.addEventListener('dragstart', function() {drag(event)}, false);
+            
+            var taskcontent = document.createElement('P');
+            // var t = document.createTextNode("Task description");
+            taskcontent.ondblclick = (ev) => {
+                //console.log(this);
+                console.log("id:" + ev.target.id);
+                taskId = ev.target.id;
+                taskDesc = prompt("Enter updated task description");
+                if (taskDesc!=null)
+                    document.getElementById(taskId).innerHTML = taskDesc;
+            }
+            var t = document.createTextNode(taskDesc);
+            taskcontent.appendChild(t);
+            taskcontent.id="task"+nexttask;     
+            taskcontent.className = 'taskcontent';
+            taskcontent.contentEditable = "false";
+            newDiv.appendChild(taskcontent);
+            
+            document.getElementsByClassName("card-body")[0].appendChild(newDiv);
+        }
+        else {
+            alert("Please enter something!");
+        }
+    }
+    else {
+        alert("Column is already at its max.")
+    }
 }
 
 function drop(ev, el) {
@@ -37,7 +66,20 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
+    // div1 = document.querySelectorAll(".div1 .divtask").length
+    // div2 = document.querySelectorAll(".div2 .divtask").length
+    // div3 = document.querySelectorAll(".div3 .divtask").length
+
+    // console.log("div1: "+div1)
+    // console.log("div2: "+div2)
+    // console.log("div3: "+div3)
+    
+    console.log(ev.target)
     ev.dataTransfer.setData("text", ev.target.id);
+    console.log(ev.target.id)
+    // compare where I was before that (store 2, 3 row before that)
+    // if it's bigger than 3 then alert and move back to it's orginal position
+
 }
 
 function deletediv(ev) {
@@ -49,5 +91,63 @@ function deletediv(ev) {
 		if (bintest == true) {
 			ev.parentNode.removeChild(ev);
 		} else {
-	}
+	    }
+}
+
+// local storage
+if (localStorage.getItem('createdBoardName') != null) {
+
+    document.getElementById("createNew").style.display = "none";
+    document.getElementById("board").style.display = "block";
+
+    createdBoardName = localStorage.getItem('createdBoardName');
+    title1 = localStorage.getItem('title1');
+    title2 = localStorage.getItem('title2');
+    title3 = localStorage.getItem('title3');
+    document.getElementById("createdBoardName").innerHTML = createdBoardName;
+    document.getElementById("title1").innerHTML = title1;
+    document.getElementById("title2").innerHTML = title2;
+    document.getElementById("title3").innerHTML = title3;
+}
+else {
+    // hide content & unhide btn
+    document.getElementById("createNew").style.display = "block";
+    document.getElementById("board").style.display = "none";
+    
+}
+
+function createNew() {
+    // create prompt to create board name and titles
+    createdBoardName = prompt("Enter board name: ");
+    title1 = prompt("Enter first column: ");
+    title2 = prompt("Enter second column: ");
+    title3 = prompt("Enter third column: ");
+
+    if (createdBoardName!=null&&title1!=null&&title2!=null&&title3!=null) {
+        localStorage.setItem('createdBoardName', createdBoardName);
+        localStorage.setItem('title1', title1);
+        localStorage.setItem('title2', title2);
+        localStorage.setItem('title3', title3);
+
+        document.getElementById("createdBoardName").innerHTML = createdBoardName;
+        document.getElementById("title1").innerHTML = title1;
+        document.getElementById("title2").innerHTML = title2;
+        document.getElementById("title3").innerHTML = title3;
+
+        document.getElementById("createNew").style.display = "none";
+        document.getElementById("board").style.display = "block";
+    }
+    else {
+        alert("Please try again!")
+    }
+}
+
+
+function updateTaskDescChanges() {
+    const input = document.getElementById('1');
+    input.addEventListener('input', updateValue);
+
+    function updateValue(e) {
+    console.log(e.target);
+    }
 }
